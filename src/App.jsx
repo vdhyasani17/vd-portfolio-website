7794901e5aaf425df46054fffd0e079c44aa8669
Vasista Dhyasani <vdhyasani@gmail.com>
2025-10-03 11:55:15 -0700

    removed vite logo and change text color (for testing)

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Vasista Dhyasani</h1>
+        <h1 className='text-sky-500'>Vasista Dhyasani</h1>
       </div>
       <p>
         This is my portfolio website
